49c3cceab5ecfd441768477d0dc088367c0d3b54
Filip Tryhuk <filip.tryhuk@student.pk.edu.pl>
2022-12-07 16:52:24 +0100

    zadania 8,9

--- src/main/java/com/example/RandomTextMessageService.java
+++ src/main/java/com/example/RandomTextMessageService.java
@@ -1,2 +1,13 @@
-package com.example;public class RandomTextMessageService {
+package com.example;
+
+import java.util.Random;
+
+public class RandomTextMessageService implements MessageService {
+    private final String[] messages = {"message 1", "sample 2", "random message 3", "mess. 4", "sent 5", "message 6/10", "Hello World", "msg 8", "9...", "last message"};
+    @Override
+    public String getMessage() {
+        Random rand = new Random(System.currentTimeMillis());
+        int roll = rand.nextInt(10);
+        return messages[roll];
+    }
 }
